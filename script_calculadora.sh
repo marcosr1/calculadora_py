dd6553b5a291771b08c36c5a370d8e921677e44b
marcosr1 <marcosrichelly43@gmail.com>
2024-10-03 12:16:09 -0300

    create um script_calculadora.sh
    script para baixar o python na sua maquina automaticamente e executar a calculadora

---
+++ script_calculadora.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#o sudo aqui seria usado para instalar o python3 em qualquer maquina caso nao tenho e posteriormente executando a calculadora autamaticamente.
+sudo apt update
+sudo apt install python3
+
+python3 calculadoraempython.py
+
+
