8267484b1a4df2c0926052971e20ce7ca4e67d79
marcosr1 <80376370+marcosr1@users.noreply.github.com>
2024-10-03 17:10:38 -0300

    Update calculadoraempython.py

--- calculadoraempython.py
+++ calculadoraempython.py
@@ -1,11 +1,11 @@
 cont = input("deseja uma operação? digite sim ou não:")
 while (cont == "sim"):
-  #colhendo informaçães
+  #Aqui colhe os dados que a calculadora vai usar na operação logo abaixo
   num1 = input('bem vindo a sua calculadora, digite um numero:')
   num2 = input('digite o outro numero que deseja:')
   op = input("qual operação deseja fazer? soma= +, subtração= -, multiplicação= *, divisão= /")
 
-  #Operaçães basicas 
+  #Aqui os dados colhidos anteriormente são certificados e usados para fazer o calculo que o usuario deseja
   if op == "+":
     resultado = int(num1) + int(num2)
     print ("O resultado é: ",resultado)
